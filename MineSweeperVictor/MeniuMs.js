2593c1e5d70e930341a531abb954348f66699d02
BuscoveanuVictor <117377902+BuscoveanuVictor@users.noreply.github.com>
2022-11-03 17:57:45 +0200

    Add files via upload

---
+++ MineSweeperVictor/MeniuMs.js
@@ -0,0 +1,21 @@
+function _(elmnt){return document.getElementById(elmnt);}
+function __(elmnt){return document.getElementsByClassName(elmnt);}
+function ___(elmnt){return document.getElementsByTagName(elmnt);}
+
+function DificultateaAleasa(nivel){
+    
+    window.location.href = 'file:///D:/xampp/htdocs/MineSweeperVictor/MS.html'
+    localStorage.removeItem('data');
+
+    switch(nivel){
+        case 'Beginner':
+            localStorage.setItem('data','incepator');
+        break;
+        case 'Intermediate':
+            localStorage.setItem('data','intermediar');
+        break;
+        case 'Expert':
+            localStorage.setItem('data','expert');
+        break;
+    }
+}
